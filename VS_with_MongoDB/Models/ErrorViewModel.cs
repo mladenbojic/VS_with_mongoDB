ffb4abe692c149bd49d3dfa7c53a7d25df873465
Mladen Bojić <mbojic@comtrade.com>
2018-10-14 15:35:00 +0200

    Add project files.

---
+++ VS_with_MongoDB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VS_with_MongoDB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
